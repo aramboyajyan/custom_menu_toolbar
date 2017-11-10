c60b0a81eb1f77da3328b43e2422bd6af1aee03e
Aram Boyajyan <dev@aram.cz>
2017-11-10 18:52:56 +0100

    initial commit

---
+++ custom_menu_toolbar.js
@@ -0,0 +1,17 @@
+
+/**
+ * @file
+ * W3b4 Toolbar JS.
+ *
+ * Created by: Topsitemakers
+ * http://www.topsitemakers.com/
+ */
+(function ($) {
+  Drupal.behaviors.w3b4Toolbar = {
+    attach: function (context, settings) {
+
+      console.log('here boss');
+
+    }
+  };
+})(jQuery);
