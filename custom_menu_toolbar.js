5f4f74b8c199f4e6f55338a6c2ab8af842da8953
topsitemakers <topsitemakers@2306644.no-reply.drupal.org>
2017-11-11 11:53:43 +0100

    Issue #2922692 by aramboyajyan: Responsive styles

--- custom_menu_toolbar.js
+++ custom_menu_toolbar.js
@@ -10,7 +10,16 @@
   Drupal.behaviors.w3b4Toolbar = {
     attach: function (context, settings) {
 
-      console.log('here boss');
+      $('#custom-menu-toolbar').once('toggle', function() {
+        $('#custom-menu-toolbar-toggle').click(function() {
+          $('#custom-menu-toolbar').toggleClass('open');
+          $('body').toggleClass('custom-menu-toolbar-open');
+        });
+        $('#custom-menu-toolbar li.expanded > a').click(function() {
+          $(this).closest('ul').toggleClass('open');
+          return false;
+        });
+      });
 
     }
   };
